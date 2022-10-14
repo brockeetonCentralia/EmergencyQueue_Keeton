4fd2ba80e96bca9f81512b2f5582cc9ac85f1fbe
Broc Keeton <broc.keeton@student.centralia.edu>
2022-10-14 12:18:39 -0700

    Add project files.

---
+++ EmergencyQueue_Keeton/EmergencyQueue_Keeton/EmergencyRoom.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+
+namespace EmergencyQueue_Keeton
+{
+
+    class EmergencyRoom
+    {
+
+        private PriorityQueue<patients, int> queue;
+        public int priority { get; set; }
+        public EmergencyRoom()
+        {
+            queue = new PriorityQueue<patients, int>();
+        }
+
+        public void Enqueue(patients patientName, int Priority)
+        {
+            queue.Enqueue(patientName, Priority);
+        }
+
+        public void patientCount()
+        {
+            Console.WriteLine("\nThere are " + queue.Count + " in line.");
+        }
+
+        public patients Dequeue()
+        {
+            return queue.Dequeue();
+        }
+
+        public patients[] patientsNames()
+        {
+
+
+            patients[] name = new patients[queue.Count];
+            for (int i = 0; i < name.Length; i++)
+            {
+
+                name[i] = queue.Dequeue();
+            }
+
+            foreach (patients patientNames in name)
+            {
+                queue.Enqueue(patientNames, priority);
+            }
+
+            foreach (patients patientNames in name)
+            {
+                Console.WriteLine(patientNames);
+            }
+            return name;
+
+        }
+
+
+        public patients[] removePatientName()
+        {
+
+
+
+            List<patients> pana = new List<patients>();
+
+            patients[] newQueue = new patients[queue.Count];
+            if (queue.Count == 0)
+            {
+                Console.WriteLine("\nQueue is clear. ");
+                return newQueue;
+            }
+
+            string delete = queue.Dequeue().ToString();
+
+
+
+            for (int i = 0; i < newQueue.Length; i++)
+            {
+                for (int j = 0; j < pana.Count; j++)
+                {
+
+                    newQueue[i] = queue.Dequeue();
+                    if (delete != null)
+                    {
+                        newQueue[i].ToString().Equals(delete);
+                        pana.RemoveAt(i);
+
+                    }
+                }
+            }
+
+            newQueue = pana.ToArray();
+            foreach (patients pname in newQueue)
+            {
+                queue.Enqueue(pname, priority);
+
+            }
+
+            return newQueue;
+        }
+    }
+}
---
+++ EmergencyQueue_Keeton/EmergencyQueue_Keeton/Program.cs
@@ -0,0 +1,102 @@
+﻿using EmergencyQueue_Keeton;
+using System;
+using System.Linq;
+using System.ComponentModel.Design;
+
+
+Console.WriteLine("Patients in Emergency Room queue: ");
+EmergencyRoom EmergencyQueue = new EmergencyRoom();
+EmergencyQueue.Enqueue(new patients("Mickey Mouse"), 2);
+EmergencyQueue.Enqueue(new patients("Fred Flintstone"), 1);
+EmergencyQueue.Enqueue(new patients("George Jetson"), 2);
+EmergencyQueue.Enqueue(new patients("Barney Rubble"), 3);
+EmergencyQueue.Enqueue(new patients("Johnny Quest"), 1);
+EmergencyQueue.patientsNames();
+EmergencyQueue.patientCount();
+
+Menu();
+
+void Menu()
+{
+    Console.WriteLine("Menu");
+    Console.WriteLine("1: Veiw List");
+    Console.WriteLine("2: Add");
+    Console.WriteLine("3: Remove");
+    Console.WriteLine("4: Exit");
+
+    string choice;
+    choice = Console.ReadLine().ToLower();
+
+    switch (choice)
+    {
+        case "1":
+            EmergencyQueue.patientsNames();
+            EmergencyQueue.patientCount();
+            Menu();
+            break;
+        
+        case "2":
+            string patientName;
+            string _Priority;
+            int priority = 0;
+            Console.WriteLine("\nType patient's first and last name:");
+            patientName = Console.ReadLine();
+            if (patientName == "")
+            {
+                while (patientName == "")
+                {
+                    Console.WriteLine("\nError: Type patient name:");
+                    patientName = Console.ReadLine();
+                }
+
+            }
+
+            Console.WriteLine("\nEnter patient's priority: 1 | 2 | 3");
+            _Priority = Console.ReadLine();
+            if (_Priority == "1" || _Priority == "2" || _Priority == "3")
+            {
+                priority = Convert.ToInt32(_Priority);
+            }
+            else
+            {
+                Console.WriteLine("Enter a correct priority number.");
+            }
+            EmergencyQueue.Enqueue(new patients(patientName), priority);
+            Console.WriteLine(patientName + " has been added with a number " + priority + " priority.");
+            patientName = "";
+            _Priority = "";
+            priority = 0;
+            EmergencyQueue.patientCount();
+            Menu();
+            break;
+
+        case "3":
+            Console.WriteLine("Press Y to remove patient from queue.");
+            string remove = Console.ReadLine();
+
+            if (remove == "Y")
+            {
+                EmergencyQueue.removePatientName();
+
+            }
+            else
+            {
+                Console.WriteLine("Rerouting to Menu");
+            }
+            EmergencyQueue.patientCount();
+            Menu();
+            break;
+
+        case "4":
+            Console.WriteLine("Exiting......");
+            break;
+
+        default:
+            Console.WriteLine("Please select a number.");
+            Menu();
+            break;
+    }
+}
+
+
+
